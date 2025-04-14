6aa3c97654836ef83cafe96319a5e3f9aaab9d27
AbhishekShah272002 <abhishek27shah@gmail.com>
2025-04-14 14:41:56 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,44 @@
+
+class Hello
+{
+public static void main(String a[]) {
+    
+    int num1 = 3;
+    int num2 = 5;
+    float marks = 6.5f;
+    double number = 1323.31113;
+    byte by = 127;
+    long l = 23232l;
+    boolean b = true;
+    int nu = 10_00_00_000;
+    double d = 12e10;
+
+
+    float f = 5.6f;
+    int t = (int) f;
+
+    int number1 = 7;
+    int number2 = 5;
+    float result1 = number1 % number2;
+    int result2 = number1 / number2;
+
+    number1 = number1 + 3;
+
+System.out.println("Hello, World!");
+System.out.println(3+5);
+System.out.println(2+9);
+System.out.println(num1 + num2);
+System.out.println(marks);
+System.out.println(number);
+System.err.println(by);
+System.out.println(l);
+System.out.println(b);
+System.out.println(nu);
+System.out.println(d);
+System.out.println(t);
+System.out.println(result1);
+System.out.println(result2);
+System.out.println(number1);
+      }
+}
+
