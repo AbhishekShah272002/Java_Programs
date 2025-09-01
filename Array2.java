0746f5f92086dd63d20ece7cc3427fec7bcb6b2b
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-01 20:12:51 +0530

    Create Array2.java

---
+++ Array2.java
@@ -0,0 +1,12 @@
+public class Array2
+  {
+    public static void main(String[] args)
+    {
+      int[] arr = new int[] {92,12,38,74 };
+
+    for(int i = 0; i < arr.length; i++)
+      System.out.println(arr[i]);
+    }
+        System.out.println();
+      }
+    }
