1091c7f311e642e8b1809e6dfe10c1879ad176b5
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-09 23:56:36 +0530

    Update SmallArray.java

--- SmallArray.java
+++ SmallArray.java
@@ -9,7 +9,7 @@ public class SmallArray{
   }
     Arrays.sort(arr); 
     for (int i = 0; i < arr.length - 1; i++){
-      if (arr[i] != arr[i+1])
+      if (arr[i] != arr[i+1]) 
       {
     return arr[i+1];
       }
