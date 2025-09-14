9d6c69e08a2e918cb899f08fe4070c7728873627
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-14 09:52:11 +0530

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,36 @@
+public class Rectangle
+{
+  double length;
+  double width;
+
+  double calculatePerimeter()
+  {
+    return (2 * length) + (2 * width);
+  }
+
+  double calculaterArea()
+  {
+    return length * width;
+  }
+
+  double getLength()
+  {
+    return length;
+  }
+  void setLength(double length)
+  {
+    this.length = length;
+  }
+
+  double getWidth()
+  {
+    return width;
+  }
+  void setWidth(double width)
+  {
+    this.width = width;
+  }
+  
+}
+
+  
