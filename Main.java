4fa7751939bf709c554ec9d74ec2613dc11f17ee
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-22 17:40:16 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ void drive()
     System.out.println("The car is driving at speed :" + speed);
   }
 }  
-
+ 
 public calss Main {  
   public static void main(String[] args)
     Car myCar = new Car();
