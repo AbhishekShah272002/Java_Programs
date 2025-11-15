71e60d57378653b40698fb31b37f6ba50671746d
Abhishek Shah <abhishek27shah@gmail.com>
2025-11-15 12:07:08 +0530

    Implement Animal and Dog classes with methods
    Fix syntax errors in Animal and Dog classes.

---
+++ Test.java
@@ -0,0 +1,23 @@
+Class Animal
+  {
+  void eat()
+    {
+    System.out.println("Animal eat food...");
+  }
+}
+  class Dog extands Animal
+    {
+    void bark()
+      {
+      Syatem.out.println("Dog Bark");
+    }
+  }
+
+public class Test
+  {
+    public Static void main(String []args)
+    Dog d = new Dog;
+    d.eat();
+    d.bark();
+  }
+}
