f84eeb0cf2199e1f95dfa37f916bfc6142874f4d
Abhishek Shah <abhishek27shah@gmail.com>
2025-11-25 22:21:56 +0530

    Fix syntax errors and enhance age validation
    Corrected syntax errors in the InvalidAge exception class and improved age checking logic in the main method.

---
+++ Try_catch.java
@@ -0,0 +1,40 @@
+class InvalidAge Exception extents Exception
+InvalidAge Exception(String message)
+{
+  super(message):;
+ }
+}
+public class Try_catch.java
+  {
+    static void checkAge(int age)
+    throw InvalidAgeAge Exception
+    {
+      if(age<18)
+      {
+        throw new InvalidAge Exception ("You must be 18 or older. Age age: " + age);
+      }
+      System.out.println("Age is fine: " + age);
+    }
+    public static void main(String[] args)
+    {
+      int [] age = {21, 16};
+      for(int age : ages)
+        {
+          try
+            {
+              checkAge(age);
+            }
+              catch(InvalidAge Exception)
+            {
+              Syatem.out.prinln("Error: " + e.get Message());
+            }
+          finally
+            {
+              System.out.println("Check finished for age: " + age + "\n");
+            }
+        }
+    }
+  }
+
+
+  
