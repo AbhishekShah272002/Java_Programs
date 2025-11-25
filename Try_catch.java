e23174cc93b3318e58dfe610c2e7ac2b21a5f256
Abhishek Shah <abhishek27shah@gmail.com>
2025-11-25 22:24:13 +0530

    Fix syntax error in checkAge method

--- Try_catch.java
+++ Try_catch.java
@@ -6,7 +6,7 @@ InvalidAge Exception(String message)
 }
 public class Try_catch.java
   {
-    static void checkAge(int age) 
+    static void checkAge(int age)  
     throw InvalidAgeAge Exception  
     {
       if(age<18)  
